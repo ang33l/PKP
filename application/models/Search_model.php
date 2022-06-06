aae05badec0bea1d1cd7e79c111521001ee56ec4
Jozef_Rusnarczyk <jrusnarczyk217@gmail.com>
2022-06-06 20:22:46 +0200

    poprawienie wyszukiwania o +/- 12h, przeniesienie edit.php z search do admin, poprawienie linkow

--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -3,19 +3,20 @@
 class Search_Model extends CI_Model {
     public function search($from, $to, $date)
     {
-        $query=$this->db->query("SELECT town, date, connection_id, stops_id FROM connections_stops WHERE town='$from' AND date>'$date';");
+        $query=$this->db->query("SELECT town, DATE_FORMAT(date, '%Y-%m-%d %H:%i') as data, connection_id, stops_id FROM connections_stops 
+        WHERE town='$from' AND date BETWEEN '$date' - INTERVAL 12 HOUR AND '$date' + INTERVAL 12 HOUR ORDER BY date;");
     
         $arr = array();
         foreach($query->result() as $row )
         {
-            $sql2=$this->db->query("SELECT town, date, connection_id, stops_id FROM connections_stops WHERE town='$to' AND connection_id='$row->connection_id';");
+            $sql2=$this->db->query("SELECT town, DATE_FORMAT(date, '%Y-%m-%d %H:%i') as data, connection_id, stops_id FROM connections_stops WHERE town='$to' AND connection_id='$row->connection_id';");
             foreach($sql2->result() as $r){
                 array_push($arr, array(
                     'from' => $row->town,
                     'to' => $r->town,
                     'connection_id' => $r->connection_id,
-                    'date_from' => $row->date,
-                    'date_to' => $r->date,
+                    'date_from' => $row->data,
+                    'date_to' => $r->data,
                     'id_start' => $row->stops_id,
                     'id_end' => $r->stops_id,
                 ));
